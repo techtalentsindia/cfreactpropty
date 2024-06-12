16d0c97c2bc16c216df7904ab8b1f3345c54f951
techtalentsindia <160840676+techtalentsindia@users.noreply.github.com>
2024-06-12 12:37:22 +0530

    Update Property.js

--- src/Painting.js
+++ src/Painting.js
@@ -1,13 +1,13 @@
-const Painting = ({ name, image, alt, artist }) => {
+const Property = ({ name, image, alt, developer }) => {
   return (
-    <figure className="painting">
+    <figure className="property">
       <img className="image-thumb" src={image} alt={alt} />
       <h4>{name}</h4>
       <p>
-        <i>{artist}</i>
+        <i>{developer}</i>
       </p>
     </figure>
   )
 }
 
-export default Painting
+export default Property
