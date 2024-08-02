9c32b9d9c45927ffa03c8d335cbe942a765d1ca0
techtalentsindia <160840676+techtalentsindia@users.noreply.github.com>
2024-08-02 17:56:01 +0530

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,6 @@ const screens = new URL('./assets/screens.svg', import.meta.url)
 
 const Content = () => {
   return (
-    <>
       <section className="river">
         <article>
           <h2>Welcome to SRS Enterprises</h2>
